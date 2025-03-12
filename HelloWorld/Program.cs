173cfb5c49b8603adb86919427861cc3cd52adcd
seokjieun <seokjieun@DESKTOP-LHBIU5O>
2025-03-12 14:03:42 +0900

    초기 커밋.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+// using : java의 import 
+
+namespace HelloWorld    
+// namespace : package 패키지 이름은 도메인을 뒤집어서 쓴다 ex) kr.hs.emirim
+{
+    class Program   
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
